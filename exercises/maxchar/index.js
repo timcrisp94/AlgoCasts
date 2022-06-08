97370f8b7735b80e1fcea1820e14e8ebfecd9a28
Tim Crisp <timothycrisp@gmail.com>
2022-06-08 14:42:54 -0500

    solve maxChar using for of and for in loops

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {}
+  let max = 0
+  let maxKey = ""
+
+  for (let char of str) {
+    if (!chars[char]) {
+      chars[char] = 1
+    } else {
+      chars[char]++
+    }
+  }
+
+  for(let char in chars) {
+    if (chars[char] > max) {
+      max = chars[char]
+      maxKey = char
+    }
+    return maxKey
+  }
+} 
+
 
 module.exports = maxChar;
