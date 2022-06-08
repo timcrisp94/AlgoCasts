fd86201e6354efb324c7616092da66377ea17573
Tim Crisp <timothycrisp@gmail.com>
2022-06-08 13:05:39 -0500

    solve for palindrome using reverse and every method

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,27 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  const reversed = str.split('').reverse().join('')
+  return reversed === str
+}
+
+
 
 module.exports = palindrome;
+
+
+/*
+
+solution two - using the every method to compare each character to its opposite [str.length - i - 1]
+
+not an ideal solution as it compares the same things more than once, but an opportunity to solve using an advanced array iterator method, and then use a more efficient method and impress your interviewer 
+
+function palindrome(str) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1]
+  })
+}
+
+*/
+
