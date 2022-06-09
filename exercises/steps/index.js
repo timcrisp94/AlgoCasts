c9ba7f83f412aa4d011b9e589947bb4690dd53fa
Tim Crisp <timothycrisp@gmail.com>
2022-06-09 14:00:41 -0500

    solve steps with recursive solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -24,19 +24,74 @@ function steps(n) {
     let stair = ''
     // from 0 to n(iterate through columns)
     for (let column = 0; column < n; column++) {
-     // if current column is equal to or less than current row
+      // if current column is equal to or less than current row
       if (column <= row) {
-      // add a # to 'stair'
-      stair += '#'
-      // else
+        // add a # to 'stair'
+        stair += '#'
+        // else
       } else {
-      // add a space to 'stair'
-      stair += ' '
+        // add a space to 'stair'
+        stair += ' '
       }
-     console.log(stair)
     }
+    console.log(stair)
   }
 }
-console.log(steps(2))
+steps(2)
 
 module.exports = steps;
+
+// recursive solution
+
+// function printNumber(n, dec = 1) {
+//   // base case
+//   if (n === 0) {
+//     return
+//   }
+//   console.log(n)
+//   printNumber(n - dec)
+// }
+
+// printNumber(10)
+
+// function steps(n, row = 0, stair = '') {
+//   if (n === row) {
+//     return
+//   }
+
+//   if (n === stair.length) {
+//     console.log(stair)
+//     return steps(n, row + 1)
+//   }
+
+//   if (stair.length <= row) {
+//     stair += '#'
+//   } else {
+//     stair += ' '
+//   }
+//   steps(n, row, stair)
+// }
+
+// steps(3)
+
+// function steps(n) {
+//   // from 0 to n (iterate through rows)
+//   for (let row = 0; row < n; row++) {
+//     // create an empty string 'stair'
+//     let stair = ''
+//     // from 0 to n(iterate through columns)
+//     for (let column = 0; column < n; column++) {
+//      // if current column is equal to or less than current row
+//       if (column <= row) {
+//       // add a # to 'stair'
+//       stair += '#'
+//       // else
+//       } else {
+//       // add a space to 'stair'
+//       stair += ' '
+//       }
+//     }
+//     console.log(stair)
+//   }
+// }
+// steps(2)
