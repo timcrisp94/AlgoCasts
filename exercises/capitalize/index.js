1171b07d413f1b2f8ace427c41e0121a4b42d5ab
Tim Crisp <timothycrisp@gmail.com>
2022-06-09 13:07:22 -0500

    solve capitalization using string iteration

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,23 +7,42 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {
-  // make an empty array words
-  const words = []
-  // split the input string by spaces to get an array
-  for (let word of str.split(' ')) {
-    // for each word in the array
-    // uppercase first letter of the word
-    // join first letter with the rest of the string
-    // push result into words array
-    words.push(word[0].toUpperCase() + word.slice(1))
-  }
-  // join words into a string and return
-  return words.join(' ')
+// function capitalize(str) {
+//   // make an empty array words
+//   const words = []
+//   // split the input string by spaces to get an array
+//   for (let word of str.split(' ')) {
+//     // for each word in the array
+//     // uppercase first letter of the word
+//     // join first letter with the rest of the string
+//     // push result into words array
+//     words.push(word[0].toUpperCase() + word.slice(1))
+//   }
+//   // join words into a string and return
+//   return words.join(' ')
   
-}
+// }
+
+
+
 
 console.log(capitalize('a short sentence'))
 
 module.exports = capitalize;
 
+// function capitalize(str) {
+//   // create an empty string called result
+//   let result = str[0].toUpperCase()
+//   // for each character in string
+//   for (let i = 1; i < str.length; i++) {
+//     // if the character to the left is a space
+//     if (str[i - 1] === ' ') {
+//       // capitalize it and add it to the result
+//       result += str[i].toUpperCase()
+//     } else {
+//       // else add it to the result
+//       result += str[i]
+//     }
+//   }
+//   return result
+// }
