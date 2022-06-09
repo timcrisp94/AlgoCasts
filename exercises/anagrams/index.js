93ff83b3a1daf4b80ec1d0ce6909aa48b2530c59
Tim Crisp <timothycrisp@gmail.com>
2022-06-09 12:24:55 -0500

    solve anagrams using sort method

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,35 +8,47 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-// function anagrams(stringA, stringB) {
-//   const aCharMap = buildCharMap(stringA)
-//   const bCharMap = buildCharMap(stringB)
+function anagrams(stringA, stringB) {
+  const aCharMap = buildCharMap(stringA)
+  const bCharMap = buildCharMap(stringB)
 
-//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-//     return false
-//   }
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false
+  }
 
-//   for (let char in aCharMap) {
-//     if (aCharMap[char] !== bCharMap[char]) {
-//       return false
-//     }
-//   }
-//   return true
-// }
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false
+    }
+  }
+  return true
+}
 
-// function buildCharMap(str) {
-//   const charMap = {}
+function buildCharMap(str) {
+  const charMap = {}
 
-//   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-//     charMap[char] = charMap[char] + 1 || 1
-//   }
+  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1
+  }
 
-//   return charMap
-// }
+  return charMap
+}
 
-function anagrams(stringA, stringB) {}
+
+
 
 
 console.log(anagrams('RAIL! SAFETY!', 'fairy tales'))
 module.exports = anagrams;
 
+// const numbers =  [10, 20, 5, -90, 10000]
+
+// console.log(numbers.sort())
+
+function anagramsTwo(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB)
+}
+
+function cleanString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+}
