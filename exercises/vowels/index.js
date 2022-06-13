eb63cce0a0fd1f57657d1e7e5aa9fe35bdfd9c1f
Tim Crisp <timothycrisp@gmail.com>
2022-06-13 10:02:08 -0500

    solve for vowels using regex

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -8,17 +8,27 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
+  const matches = str.match(/[aeiou]/gi)
+  // g makes sure we don't stop at the first match
+  // i stands for case insensitive
+  return matches ? matches.length : 0
+}
+
+
+console.log(vowels('Hi there!'))
+module.exports = vowels;
+
+/*
+function vowels(str) {
   let count = 0
   const checker = ['a', 'e', 'i', 'o', 'u']
-  // iterate
+  / iterate
   for (let char of str.toLowerCase()) {
     if (checker.includes(char)) {
       count++
     }
   }
-  // return number of vowels
+  / return number of vowels
   return count
 }
-
-console.log(vowels('Hi there!'))
-module.exports = vowels;
+*/
