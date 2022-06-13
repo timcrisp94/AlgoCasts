bf00fc2fd2b7a1bf5e97078e929d13ebaf91fbeb
Tim Crisp <timothycrisp@gmail.com>
2022-06-13 09:58:30 -0500

    solve vowels with iteration

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,18 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let count = 0
+  const checker = ['a', 'e', 'i', 'o', 'u']
+  // iterate
+  for (let char of str.toLowerCase()) {
+    if (checker.includes(char)) {
+      count++
+    }
+  }
+  // return number of vowels
+  return count
+}
 
+console.log(vowels('Hi there!'))
 module.exports = vowels;
