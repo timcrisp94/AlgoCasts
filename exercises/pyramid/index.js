274c37386b08c034eba0083c0214e143a6e07e5b
Tim Crisp <timothycrisp@gmail.com>
2022-06-13 07:30:53 -0500

    solve pyramid problem with iteration and recursion

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -23,21 +23,52 @@
   // console.log(stair)
 
   // compare n to columns
-  // n * 2 - 1 = columns
+    // n * 2 - 1 = columns
 
-function pyramid(n) {
-  for (let row = 0; row < n; row++) {
-    let level = ''
-    for (let column = 0; column < 2 * n - 1; column++) {
-      if (column <= row) {
-        level += '#'
-      } else {
-        level += ' '
-      }
-    }
-    console.log(level)
+  // calculate midpoint of column
+
+
+
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2)
+  
+//   for (let row = 0; row < n; row++) {
+//     let level = ''
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       // check that current column is within the bounds of 
+//       // midpoint - row and midpoint + row
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += '#'
+//       } else {
+//         level += ' '
+//       }
+//     }
+//     console.log(level)
+//   }
+// }
+
+
+function pyramid(n, row = 0, level = '') {
+  // base case
+  if (row === n) {
+    return
   }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level)
+    return pyramid(n, row + 1)
+  }
+
+  const midpoint = Math.floor((2 * n - 1) / 2)
+  let add
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#'
+  } else {
+    add = ' '
+  }
+  pyramid(n, row, level + add)
 }
+
 
 pyramid(3)
 
@@ -46,3 +77,24 @@ pyramid(3)
 
 module.exports = pyramid;
 
+
+/*
+-- recursive solution -- 
+
+const columns = [0, 1, 2, 3, 4]
+const row = 0
+
+/ number of elements divided by 2,
+/ so for above: 
+Math.floor ((5 / 2))
+
+/ and if row goes to 1:
+Math.floor ((5 / 2)) >> 2
+Math.floor ((5 / 2)) + 1 >> 3
+Math.floor ((5 / 2)) - 1 >> 1
+/ gives us # at indexes 1, 2, 3
+
+/ and the formula is"
+Math.floor((2 * n - 1 / 2))
+
+*/
