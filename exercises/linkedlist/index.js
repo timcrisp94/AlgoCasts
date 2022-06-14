6b7fa4811bc0631f23ad46202e73d38f7613822f
Tim Crisp <timothycrisp@gmail.com>
2022-06-14 13:49:48 -0500

    add getLast method to linked list

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -29,8 +29,22 @@ class LinkedList {
     return counter
   }
 
-  getFirst(Node) {
+  getFirst() {
     return this.head
+  }
+
+  getLast() {
+    if (!this.head) {
+      return null
+    }
+
+    let node = this.head;
+    while (node) {
+      if (!node.next) {
+        return node;
+      }
+      node = node.next;
+    }
   }
 }
 
