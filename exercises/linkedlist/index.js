8fbf14f3fb37aed4a428ceb4aed0185730b5bdb2
Tim Crisp <timothycrisp@gmail.com>
2022-06-14 14:29:05 -0500

    add getAt method to linked list

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -89,7 +89,21 @@ class LinkedList {
       this.head = new Node(data)
     }
   }
-  
+
+  getAt(index) {
+    let node = this.head
+    let counter = 0
+    while (node) {
+      if (counter === index) {
+        return node
+      }
+
+      counter++
+      node = node.next
+    }
+    return null
+  }
+
 }
 
 
