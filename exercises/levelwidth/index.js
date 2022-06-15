15e83d89c7c1cb8e8b178ca318bd45e13bdbba2e
Tim Crisp <timothycrisp@gmail.com>
2022-06-15 17:06:02 -0500

    solve level width

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,24 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  // width = breadth first
+  const counters = [0]
+  const array = [root, 's']
+
+  while (array.length > 1) {
+    const node = array.shift()
+
+    if (node === 's') {
+      counters.push(0)
+      array.push('s')
+    } else {
+      array.push(...node.children)
+      counters[counters.length - 1]++
+    }
+  }
+  return counters
+
+}
 
 module.exports = levelWidth;
