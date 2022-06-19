1bdcf1a844b49476826625fab309db775e7799b5
Tim Crisp <timothycrisp@gmail.com>
2022-06-19 13:14:39 -0500

    implement merge sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -11,6 +11,7 @@ function bubbleSort(arr) {
       }
     }
   }
+
   return arr
 }
 
@@ -35,11 +36,29 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr
+  }
 
+  const center = Math.floor(arr.length / 2)
+  const left = arr.slice(0, center)
+  const right = arr.slice(center)
+
+  return merge(mergeSort(left), mergeSort(right))
 }
 
 function merge(left, right) {
+  const results = []
 
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift())
+    } else {
+      results.push(right.shift())
+    }
+  }
+
+  return [...results, ...left, ...right]
 }
 
 
